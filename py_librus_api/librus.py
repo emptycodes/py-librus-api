f22f822baf5cd1d19fcfc9f8f5e1b59efcabcede
TheAmazingRak <tomek.niezurawski@gmail.com>
2018-11-10 14:21:53 +0100

    Fixed Issue #2 bug, removed unnecessary make_connection function
    Fixed bug in which user couldn't log in twice / two users couldn't log in in the same time.
    Also merged login() and make_connection() together.

--- py_librus_api/librus.py
+++ py_librus_api/librus.py
@@ -23,44 +23,62 @@ class Librus:
     attendances = None
     attendances_types = None
 
+    def __init__(self):
+        self.reset()
+
+    # Resets class
+    def reset(self):
+        self.headers = {
+            "Authorization": "Basic Mjg6ODRmZGQzYTg3YjAzZDNlYTZmZmU3NzdiNThiMzMyYjE="
+        }
+        self.logged_in = False
+
+        self.lucky_number = None
+        self.grades = None
+        self.subjects = None
+        self.categories = None
+        self.students = None
+        self.teachers = None
+        self.comments = None
+        self.lessons = None
+        self.school_free_days = None
+        self.teacher_free_days = None
+        self.teacher_free_days_types = None
+        self.attendances = None
+        self.attendances_types = None
+
     # Checks data and decides method of login
     def login(self, login, password):
         if not self.logged_in:
             if login is None or password is None or login == "" or password == "":
                 return False
             else:
-                if self.make_connection(login, password):
-                    return True
-                else:
-                    return False
+                """Make connection to the host and get auth token"""
+                r = None
+                loop = 0
+                while r is None:
+                    try:
+                        r = requests.post(self.host + "OAuth/Token", data={"username": login,
+                                                                           "password": password,
+                                                                           "librus_long_term_token": "1",
+                                                                           "grant_type": "password"},
+                                          headers=self.headers)
 
-    # Make connection and get access token
-    def make_connection(self, login, password):
-        r = None
-        loop = 0
-        while r is None:
-            try:
-                r = requests.post(self.host + "OAuth/Token", data={"username": login,
-                                                                   "password": password,
-                                                                   "librus_long_term_token": "1",
-                                                                   "grant_type": "password"},
-                    headers=self.headers)
+                        if r.ok:
+                            self.logged_in = True
+                            self.headers["Authorization"] = "Bearer " + r.json()["access_token"]
 
-                if r.ok:
-                    self.logged_in = True
-                    self.headers["Authorization"] = "Bearer " + r.json()["access_token"]
-
-                    return True
-                else:
-                    return False
-            except requests.exceptions.Timeout:
-                if loop >= 10:
-                    return False
-                else:
-                    loop += 1
-                    continue
-            except requests.exceptions.RequestException:
-                raise requests.exceptions.ConnectionError
+                            return True
+                        else:
+                            return False
+                    except requests.exceptions.Timeout:
+                        if loop >= 10:
+                            return False
+                        else:
+                            loop += 1
+                            continue
+                    except requests.exceptions.RequestException:
+                        raise requests.exceptions.ConnectionError
 
     def get_data(self, url):
         if self.logged_in:
@@ -68,7 +86,7 @@ class Librus:
                 return requests.get(self.host + "2.0/" + url, headers=self.headers)
             except (requests.exceptions.ConnectionError, TimeoutError, requests.exceptions.Timeout,
                     requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout):
-                    raise Exception("Connection error")
+                raise Exception("Connection error")
         else:
             raise Exception("User not logged in")
 
@@ -76,7 +94,7 @@ class Librus:
         if self.lucky_number is None:
             r = self.get_data("LuckyNumbers")
             self.lucky_number = r.json()["LuckyNumber"]["LuckyNumber"]
-            
+
             return self.lucky_number
 
         return self.lucky_number
